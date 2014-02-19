e34681bf1324fa6a220b90479ccecdad88a36f9b
Allen Wu <allenwu1018@gmail.com>
2014-02-19 20:43:17 +0800

    model attributes and new
    Ruby Bootcamp D2: Basic Document
    
    * instantiate a model with attributes
    * read, write attribute
    * get all attributes
    * a mock `new_record?` method

--- lib/my_mongoid/document.rb
+++ lib/my_mongoid/document.rb
@@ -5,6 +5,35 @@ module MyMongoid
       base.extend ClassMethods
       MyMongoid.register_model(base)
     end
+
+    def initialize(attrs = {})
+      raise ArgumentError if !attrs.is_a? Hash
+      #attrs ||= {}
+      if !attrs.empty?
+        attrs.each_pair do |key, value|
+          attributes[key] = value
+        end
+      end
+    end
+
+    def attributes
+      @attributes ||= {}
+    end
+
+    def read_attribute(key)
+      # raise UnknownAttribute Error if key doesn't exist
+      @attributes[key]
+    end
+
+    def write_attribute(key, value)
+      # raise UnknownAttribute Error if key doesn't exist
+      @attributes[key] = value
+    end
+
+    def new_record?
+      true
+    end
+
     module ClassMethods
       def is_mongoid_model?
         # refactor?
