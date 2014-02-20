671fcb9162abd63bc37f7b2ba3f18164703b5e58
Allen Wu <allenwu1018@gmail.com>
2014-02-21 00:23:09 +0800

    setup rspec

---
+++ spec/my_mongoid/document_spec.rb
@@ -0,0 +1,17 @@
+describe MyMongoid::Document do
+  it "is a module" do
+    expect(MyMongoid::Document).to be_a(Module)
+  end
+
+  describe ".new" do
+  end
+
+  describe "#read_attribute" do
+  end
+
+  describe "#write_attribute" do
+  end
+
+  describe "#new_record?" do
+  end
+end
